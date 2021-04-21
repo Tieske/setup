480064fdc939896663c6200d142e6409d434332c
Ubuntu <ubuntu@ip-172-31-15-209.us-east-2.compute.internal>
2021-04-21 08:57:55 +0000

    initial commit

---
+++ ubuntu-kong-dev.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# originally written for an AWS EC2 instance running Ubuntu
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https curl lsb-core httpie
+
+# Kong specific
+echo "deb https://kong.bintray.com/kong-deb `lsb_release -sc` main" | sudo tee -a /etc/apt/sources.list
+curl -o bintray.key https://bintray.com/user/downloadSubjectPublicKey?username=bintray
+sudo apt-key add bintray.key
+rm bintray.key
+sudo apt-get update
+sudo apt-get install -y kong
+
+# symlink 'lua' to luajit
+sudo ln -s /usr/local/openresty/luajit/bin/luajit /usr/local/openresty/luajit/bin/lua
+
+# add luajit to path
+echo 'export PATH="/usr/local/openresty/luajit/bin:$PATH"' >> "$HOME/.profile"
+
+# add resty to path
+echo 'export PATH="/usr/local/openresty/bin:$PATH"' >> "$HOME/.profile"
+
